d123335a424fc091f8332d780ffd608c28058100
Briana Oursler <boursler@blackhole.localdomain>
2018-03-09 14:46:17 -0700

    Add submit button to page, add animate functionality, fix bug in which click method did not grab dynamically generated buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 var apiKey = "McNSC8SBAR4eCXtt4Ijlttss6Pasj2Pm";
-var btn;
+
+
 function setButtons() {
-	var topics = ["Death Note", "Girls Bravo", "Azumanga Daioh", "Attack on Titan", "Dragon Ball Z"];
+	var topics = ["Death Note", "The Ancient Magus Bride", "Azumanga Daioh", "Attack on Titan", "Dragon Ball Z", "Unlimited Blade Works"];
 
 	for (var i = 0; i < topics.length; i++) {
 		btn = $("<button class='btn btn-dark'>");
@@ -24,13 +25,18 @@ function getData() {
 		console.log(response);
 		for (var i = 0; i < 10; i++) {
 			var img = $("<img>");
-			var still_gif = response.data[i].images.fixed_height_still.url;
-			var animated_gif = response.data[i].images.fixed_height.url;
+			still_gif = response.data[i].images.fixed_height_still.url;
+			animated_gif = response.data[i].images.fixed_height.url;
 			var rating = response.data[i].rating;
 			console.log(rating);
 			var p = $("<p>");
 			console.log(still_gif);
 			img.attr("src", still_gif);
+			img.attr("data-state", "still");
+			img.attr("data-still", still_gif);
+			img.attr("data-animate", animated_gif);
+			img.addClass("img");
+
 			p.text("Rating: " + rating);
 			$("#gifsDiv").prepend(p);
 			$("#gifsDiv").prepend(img);
@@ -38,6 +44,25 @@ function getData() {
 
 	});
 }
+function interact() {
+	console.log("interact phase");
+	if ($(this).attr("data-state") === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	}
+}
+// id="newButton-input
+// input id="add-anime"
+$("#add-anime").click(function addButton() {
+	event.preventDefault();
+	var newAnime = $("#newButton-input").val().trim();
+	btn = $("<button class='btn btn-dark'>");
+	btn.text(newAnime);
+	btn.attr("data-name", newAnime);
+	$("#btnDiv").append(btn);
+	console.log(newAnime + "anime trying to add");
+});
 
 setButtons();
-$(".btn").click(getData);
+$(document).on("click", "button", getData);
+$(document).on("click", "img", interact);
