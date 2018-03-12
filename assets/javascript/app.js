8c30428d25fee251e4bf888404f9653c44a47fbb
Briana Oursler <boursler@blackhole.localdomain>
2018-03-11 20:26:23 -0700

    Add logic for fewer than 10 results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,13 @@ var apiKey = "McNSC8SBAR4eCXtt4Ijlttss6Pasj2Pm";
 
 
 function setButtons() {
-	var topics = ["Death Note", "The Ancient Magus Bride", "Azumanga Daioh", "Attack on Titan", "Dragon Ball Z", "Unlimited Blade Works"];
+	var topics = ["Death Note", "Code Geass", "Naruto", "Attack on Titan", "Dragon Ball Z", "Unlimited Blade Works"];
 
 	for (var i = 0; i < topics.length; i++) {
 		btn = $("<button class='btn btn-dark'>");
 		btn.text(topics[i]);
 		btn.attr("data-name", topics[i]);
+		btn.addClass("nav-item");
 		$("#btnDiv").append(btn);
 
 
@@ -24,23 +25,31 @@ function getData() {
 		method: 'GET'
 	}).done(function (response) {
 		console.log(response);
-		for (var i = 0; i < 10; i++) {
-			var img = $("<img>");
-			still_gif = response.data[i].images.fixed_height_still.url;
-			animated_gif = response.data[i].images.fixed_height.url;
-			var rating = response.data[i].rating;
-			console.log(rating);
-			var p = $("<p>");
-			console.log(still_gif);
-			img.attr("src", still_gif);
-			img.attr("data-state", "still");
-			img.attr("data-still", still_gif);
-			img.attr("data-animate", animated_gif);
-			img.addClass("img");
+		if (response.data.length >= 10) {
+			for (var i = 0; i < 10; i++) {
+				var img = $("<img>");
+				still_gif = response.data[i].images.fixed_height_still.url;
+				animated_gif = response.data[i].images.fixed_height.url;
+				var rating = response.data[i].rating;
+				console.log(rating);
+				var p = $("<p>");
+				console.log(still_gif);
+				img.attr("src", still_gif);
+				img.attr("data-state", "still");
+				img.attr("data-still", still_gif);
+				img.attr("data-animate", animated_gif);
+				img.addClass("img");
 
-			p.text("Rating: " + rating);
-			$("#gifsDiv").prepend(p);
-			$("#gifsDiv").prepend(img);
+				p.text("Rating: " + rating);
+				var gifContainer = $("<div id='gifContainer'>");
+				$(gifContainer).prepend(p);
+				$(gifContainer).prepend(img);
+				$(gifContainer).addClass("col-md-6");
+				$("#gifsDiv").prepend(gifContainer);
+			}
+		}
+		else {
+			$("#gifsDiv").text("Please choose another anime, this anime is not popular enough with Giphy.");
 		}
 
 	});
@@ -64,6 +73,7 @@ $("#add-anime").click(function addButton() {
 	btn = $("<button class='btn btn-dark'>");
 	btn.text(newAnime);
 	btn.attr("data-name", newAnime);
+	btn.addClass("nav-item");
 	$("#btnDiv").append(btn);
 	console.log(newAnime + "anime trying to add");
 });
