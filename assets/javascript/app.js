f7bcef9ce95e9e1fb4a47b6984045792381b0145
Briana Oursler <boursler@blackhole.localdomain>
2018-03-11 20:57:44 -0700

    Make makeButton function to prevent code rewriting and create boolean lengthCheck function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,21 +2,28 @@ var apiKey = "McNSC8SBAR4eCXtt4Ijlttss6Pasj2Pm";
 
 
 function setButtons() {
+	//create initial array of buttons
 	var topics = ["Death Note", "Code Geass", "Naruto", "Attack on Titan", "Dragon Ball Z", "Unlimited Blade Works"];
-
+	//make a button for each member of the initial array
 	for (var i = 0; i < topics.length; i++) {
-		btn = $("<button class='btn btn-dark'>");
-		btn.text(topics[i]);
-		btn.attr("data-name", topics[i]);
-		btn.addClass("nav-item");
-		$("#btnDiv").append(btn);
-
-
+		makeButton(topics[i]);
 	}
 }
+
+//prints a button to the page with text and data-name attribute passed-in
+function makeButton(animeTitle) {
+	btn = $("<button class='btn btn-dark'>");
+	btn.text(animeTitle);
+	btn.attr("data-name", animeTitle);
+	btn.addClass("nav-item");
+	$("#btnDiv").append(btn);
+}
+
+//function to make Ajax call
 function getData() {
-	console.log("hello");
+	//empty the gifsDiv so that it can be filled anew
 	$("#gifsDiv").empty();
+	//remove extra spaces to the left and right of the data-name attribute, then replace spaces between words with '+' for Giphy search
 	var search = $(this).attr("data-name").trim().split(" ").join("+");
 	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + search + "&limit=10";
 	console.log("url = " + queryURL);
@@ -24,8 +31,10 @@ function getData() {
 		url: queryURL,
 		method: 'GET'
 	}).done(function (response) {
+		//callback function that takes giphy data, sets data attributes and prints to the page
 		console.log(response);
-		if (response.data.length >= 10) {
+		//check length of response & print if length matches 10
+		if (lengthCheck(response)) {
 			for (var i = 0; i < 10; i++) {
 				var img = $("<img>");
 				still_gif = response.data[i].images.fixed_height_still.url;
@@ -39,7 +48,6 @@ function getData() {
 				img.attr("data-still", still_gif);
 				img.attr("data-animate", animated_gif);
 				img.addClass("img");
-
 				p.text("Rating: " + rating);
 				var gifContainer = $("<div id='gifContainer'>");
 				$(gifContainer).prepend(p);
@@ -49,11 +57,21 @@ function getData() {
 			}
 		}
 		else {
-			$("#gifsDiv").text("Please choose another anime, this anime is not popular enough with Giphy.");
+			//Print an error message and ask the user to select another anime
+			$("#gifsDiv").text("Please choose another anime.");
 		}
-
 	});
 }
+
+function lengthCheck(response) {
+	if (response.data.length === 10) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 function interact() {
 	console.log("interact phase");
 	if ($(this).attr("data-state") === "still") {
@@ -65,17 +83,11 @@ function interact() {
 		$(this).attr("data-state", "still");
 	}
 }
-// id="newButton-input
-// input id="add-anime"
+
 $("#add-anime").click(function addButton() {
 	event.preventDefault();
 	var newAnime = $("#newButton-input").val().trim();
-	btn = $("<button class='btn btn-dark'>");
-	btn.text(newAnime);
-	btn.attr("data-name", newAnime);
-	btn.addClass("nav-item");
-	$("#btnDiv").append(btn);
-	console.log(newAnime + "anime trying to add");
+	makeButton(newAnime);
 });
 
 $(document).ready(function () {
